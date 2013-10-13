86d5e38c294ed9d88157612532840bf594bb5d09
richard <richard.s.shepherd@gmail.com>
2013-10-13 21:36:28 +0100

    -

---
+++ Board.py
@@ -0,0 +1,16 @@
+
+class Board(object):
+    '''
+    Represents the board.
+
+    Holds a collection of objects which each implement the
+    (implicit) Square interface, as well as the collection
+    of Chance and Community Chest cards.
+    '''
+
+    def __init__(self):
+        '''
+        The 'constructor'.
+        '''
+        pass
+
--- Street.py
+++ Street.py
@@ -7,15 +7,27 @@ class Street(Property):
     A type of Property representing a street.
 
     Manages rents, house-building and so on.
+
+    Implements the (implicit) Square interface.
     '''
 
-    Rents = namedtuple("Rents", "base_rent set_rent ") # TODO: add house rents
+    class Rents(namedtuple("Rents", "base whole_set one_house two_houses three_houses four_houses hotel")):
+        '''
+        Holds the various rents for a Street property.
+        '''
+        pass
 
     def __init__(self, name, set, price, mortgage_value, house_price, rents):
         '''
         The 'constructor'.
 
-        rents: passed in as a Rents namedtuple
+        rents: passed in as a Rents object (a namedtuple).
         '''
+        # The base class holds the values applicable to all properties...
+        super(Street, self).__init__(name, set, price)
 
+        # We hold the ones specific to streets...
+        self.mortgage_value = mortgage_value
+        self.house_price = house_price
+        self.rents = rents
 
