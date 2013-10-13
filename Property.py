a5cc7cb447f254bddf45cc7fe32ade0a8852f115
richard <richard.s.shepherd@gmail.com>
2013-10-13 18:24:17 +0100

    -

---
+++ Property.py
@@ -0,0 +1,43 @@
+
+class Property(object):
+    '''
+    A base class for properties, which manages some common features
+    of them.
+
+    This include holding whether the property is owned (and if so, who by)
+    and whether the property is mortgaged.
+
+    Derived classes are: Street, Station, Utility.
+    '''
+
+    class Set(object):
+        '''
+        An 'enum' for the different property sets.
+        '''
+        INVALID = 0
+        BROWN = 1
+        LIGHT_BLUE = 2
+        PURPLE = 3
+        ORANGE = 4
+        RED = 5
+        YELLOW = 6
+        GREEN = 7
+        DARK_BLUE = 8
+        STATION = 9
+        UTILITY = 10
+
+
+    def __init__(self, name, set, price, mortgage_value):
+        '''
+        The 'constructor'.
+        '''
+        self.name = name
+        self.set = set
+        self.price = price
+        self.mortgage_value = mortgage_value
+        self.mortgaged = False
+        self.owner = None
+
+
+
+
