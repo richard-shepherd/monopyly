6a67884e84c9b998b4931affcfe38223f6b01209
richard <richard.s.shepherd@gmail.com>
2013-12-14 16:04:48 +0000

    monopyly

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from monopyly import *
 
 # We set up the logger...
-Logger.add_handler(ConsoleLogHandler(Logger.INFO))
+Logger.add_handler(ConsoleLogHandler(Logger.INFO_PLUS))
 
 # We find the collection of AIs from the AIs folder...
 ais = load_ais()
 
-# We play a tournament. This plays all the permutations of players
-# against each other...
-tournament = Tournament(player_ais=ais, max_players_per_game=4, number_of_rounds=1)
+# We play a tournament. This plays all the permutations of
+# AIs against each other...
+tournament = Tournament(player_ais=ais, max_players_per_game=4, number_of_rounds=100)
 results = tournament.play()
 print(results)
 
