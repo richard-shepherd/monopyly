11bf3a025f93c979d6ce806d884284ebea8d4886
richard <richard.s.shepherd@gmail.com>
2014-01-12 21:27:42 +0000

    monopyly

---
+++ gui/Monopyly/Monopyly.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace gui
+{
+    public partial class Monopyly : Form
+    {
+        public Monopyly()
+        {
+            InitializeComponent();
+        }
+    }
+}
