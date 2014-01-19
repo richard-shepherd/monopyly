e2364c16ebf635f8a7199a368162650e9654a98e
richard <richard.s.shepherd@gmail.com>
2014-01-19 16:40:09 +0000

    monopyly

--- protos/_generate_code.cmd
+++ protos/_generate_code.cmd
@@ -1,5 +1,13 @@
 
-:: The StartOfTournament object...
-protoc -I=. --python_out=. StartOfTournament.proto
-protogen -i:StartOfTournament.proto -o:StartOfTournament.cs
+:: The StartOfTournamentMessage object...
+protoc -I=. --python_out=. StartOfTournamentMessage.proto
+protogen -i:StartOfTournamentMessage.proto -o:StartOfTournamentMessage.cs
+
+:: The PlayerInfoMessage object...
+protoc -I=. --python_out=. PlayerInfoMessage.proto
+protogen -i:PlayerInfoMessage.proto -o:PlayerInfoMessage.cs
+
+:: The BoardUpdateMessage object...
+protoc -I=. --python_out=. BoardUpdateMessage.proto
+protogen -i:BoardUpdateMessage.proto -o:BoardUpdateMessage.cs
 
